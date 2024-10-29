2739a46e59a78be7ac8b60fc0f006bd6be3d4927
karlghosn1 <karl.ghosn.dev@gmail.com>
2024-10-29 19:41:08 +0200

    videos fix

--- src/components/details_components/videos/VideoList.tsx
+++ src/components/details_components/videos/VideoList.tsx
@@ -1,9 +1,29 @@
 import { useEffect, useRef, useState } from "react";
 import { VideoInterface } from "../../../models/mediaModel";
-
 import classes from "./video.module.css";
-import VideoItem from "./VideoItem";
 import ListArrows from "../../horizontal_list/arrows/ListArrows";
+import { NavLink } from "react-router-dom";
+import MotionDiv from "../../ui/MotionDiv";
+import { IoPlayCircleOutline } from "react-icons/io5";
+
+const VideoItem = ({ video }: { video: VideoInterface }) => {
+  return (
+    <MotionDiv>
+      <NavLink
+        target="_blank"
+        to={`https://www.youtube.com/watch?v=${video.key}`}
+        className={classes["video__item"]}
+      >
+        <img
+          alt={video.name}
+          src={`https://i.ytimg.com/vi/${video.key}/hqdefault.jpg`}
+        />
+
+        <IoPlayCircleOutline className={classes["video__item--play"]} />
+      </NavLink>
+    </MotionDiv>
+  );
+};
 
 const VideoList = ({ videos }: { videos: VideoInterface[] }) => {
   const listRef = useRef<HTMLUListElement>(null);
@@ -18,7 +38,7 @@ const VideoList = ({ videos }: { videos: VideoInterface[] }) => {
         <h1 className="homepage-title">Videos</h1>
         <ListArrows listRef={ref} />
       </div>
-      <ul ref={listRef} className={classes["video-container"]}>
+      <ul ref={listRef} className={classes["videos"]}>
         {videos.map((video) => (
           <VideoItem key={video.key} video={video} />
         ))}
