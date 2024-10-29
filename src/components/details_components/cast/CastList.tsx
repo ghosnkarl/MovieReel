c13ca47fc705066a5b0cc041f310a00184ce6cf6
karlghosn1 <karl.ghosn.dev@gmail.com>
2024-10-29 19:03:39 +0200

    cast list fix

--- src/components/details_components/cast/CastList.tsx
+++ src/components/details_components/cast/CastList.tsx
@@ -4,19 +4,35 @@ import classes from "./cast.module.css";
 import { getProfileImage } from "../../../helpers/imageSizes";
 import { useEffect, useRef, useState } from "react";
 import ListArrows from "../../horizontal_list/arrows/ListArrows";
-import CastItem from "./CastItem";
 
 import { IoChevronForward } from "react-icons/io5";
 
-const CastList = ({
-  title,
-  image,
-  credits,
-}: {
+interface CastItemProps {
+  link: string;
+  image: string;
+  title: string;
+  text: string;
+}
+
+const CastItem = ({ link, image, title, text }: CastItemProps) => {
+  return (
+    <NavLink to={link} className={classes["cast__item"]}>
+      <img className={classes["cast__item--profile"]} alt={title} src={image} />
+      <div>
+        <h2 className={classes["cast__item--name"]}>{title}</h2>
+        <p className={classes["cast__item--character"]}>{text}</p>
+      </div>
+    </NavLink>
+  );
+};
+
+interface CastListProps {
   title: string;
   image: string;
   credits: { cast: CastInterface[]; crew: CrewInterface[] };
-}) => {
+}
+
+const CastList = ({ title, image, credits }: CastListProps) => {
   let castList = [...credits.cast];
   if (castList.length > 20) castList = castList.slice(0, 20);
   const listRef = useRef<HTMLUListElement>(null);
@@ -25,6 +41,7 @@ const CastList = ({
   useEffect(() => {
     setRef(listRef.current);
   }, [listRef]);
+
   return (
     <div>
       <div className="list-header">
@@ -38,15 +55,16 @@ const CastList = ({
         </NavLink>
         <ListArrows listRef={ref} />
       </div>
-      <ul ref={listRef} className={classes["cast-container"]}>
+      <ul ref={listRef} className={classes.cast}>
         {castList.map((cast) => (
-          <CastItem
-            key={cast.credit_id}
-            link={`/people/${cast.id}`}
-            image={getProfileImage(cast.profile_path, "w185")}
-            title={cast.name}
-            text={cast.character}
-          />
+          <li key={cast.credit_id}>
+            <CastItem
+              link={`/people/${cast.id}`}
+              image={getProfileImage(cast.profile_path, "w185")}
+              title={cast.name}
+              text={cast.character}
+            />
+          </li>
         ))}
       </ul>
     </div>
