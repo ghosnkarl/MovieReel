7e88fdd2a53ca4fb07a390e361284fb0ef991dff
karlghosn1 <karl.ghosn.dev@gmail.com>
2024-11-07 06:43:15 +0200

    details header fix

--- src/components/details_components/DetailsHeader.tsx
+++ src/components/details_components/DetailsHeader.tsx
@@ -3,6 +3,7 @@ import classes from '../../styles/details-header.module.css';
 
 import { GenreInterface } from '../../models/genreModel';
 import RatingStar from '../rating/RatingStar';
+import { getBackdropImage } from '../../helpers/imageSizes';
 
 interface DetailsHeaderProps {
   title: string;
@@ -11,6 +12,7 @@ interface DetailsHeaderProps {
   genres: GenreInterface[];
   vote_average: number;
   overview: string;
+  backdrop_path: string | null;
 }
 
 const DetailsHeader = ({
@@ -20,28 +22,37 @@ const DetailsHeader = ({
   genres,
   vote_average,
   overview,
+  backdrop_path,
 }: DetailsHeaderProps) => {
   return (
     <div className={classes.header}>
-      <p className={classes['header__genres']}>
-        {genres.map((genre) => genre.name).join(' • ')}
-      </p>
-      <h1 className={classes['header__title']}>{title}</h1>
-      <div className={classes['header__details']}>
-        <RatingStar value={vote_average} size='medium' />
-        <p className={classes['header__date']}>
-          {moment(release_date).format('MMM DD, YYYY')}
-          {runtime !== 0 && (
-            <>
-              {' • '}
-              <span>
-                {Math.floor(runtime / 60)}h {runtime % 60}m
-              </span>
-            </>
-          )}
+      <img
+        className={classes['backdrop-img']}
+        src={getBackdropImage(backdrop_path, 'w1280')}
+        alt={title}
+      />
+      <div className={classes['header__text--container']}>
+        <p className={classes['header__genres']}>
+          {genres.map((genre) => genre.name).join(' • ')}
         </p>
+        <h1 className={classes['header__title']}>{title}</h1>
+        <div className={classes['header__details']}>
+          <RatingStar value={vote_average} size='medium' />
+          <p className={classes['header__date']}>
+            {moment(release_date).format('MMM DD, YYYY')}
+            {runtime !== 0 && (
+              <>
+                {' • '}
+                <span>
+                  {Math.floor(runtime / 60)}h {runtime % 60}m
+                </span>
+              </>
+            )}
+          </p>
+        </div>
+
+        <p className={classes['header__overview']}>{overview}</p>
       </div>
-      <p className={classes['header__overview']}>{overview}</p>
     </div>
   );
 };
--- src/pages/movies/MovieDetails.tsx
+++ src/pages/movies/MovieDetails.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { fetchSingleResult } from '../../services/http';
-import { getBackdropImage } from '../../helpers/imageSizes';
 import classes from '../../styles/movie-details.module.css';
 import ScrollToTop from '../../components/ui/ScrollToTop';
 import DetailsHeader from '../../components/details_components/DetailsHeader';
@@ -31,11 +30,6 @@ const MovieDetails = () => {
   if (movie) {
     content = (
       <>
-        <img
-          className={classes['backdrop-img']}
-          src={getBackdropImage(movie.backdrop_path, 'w1280')}
-          alt={movie.title}
-        />
         <DetailsHeader
           title={movie.title}
           overview={movie.overview}
@@ -43,6 +37,7 @@ const MovieDetails = () => {
           vote_average={movie.vote_average}
           release_date={movie.release_date}
           runtime={movie.runtime}
+          backdrop_path={movie.backdrop_path}
         />
         <div className={classes['details-container']}>
           <DetailsMainContainer movie={movie} />
