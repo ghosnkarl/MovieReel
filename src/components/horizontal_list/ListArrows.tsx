28f81a3a724f0f5928f57146fecadfafe8488946
karlghosn1 <karl.ghosn.dev@gmail.com>
2024-11-18 20:14:06 +0200

    horizontal list fixes

--- src/components/horizontal_list/ListArrows.tsx
+++ src/components/horizontal_list/ListArrows.tsx
@@ -1,26 +1,60 @@
+import { useState } from 'react';
 import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md';
+import { motion } from 'framer-motion';
 
 interface IListArrows {
   listRef: HTMLUListElement | null;
 }
 
 const ListArrows = ({ listRef }: IListArrows) => {
+  const [scrollPosition, setScrollPosition] = useState(0); // Track the scroll position
+
   const handleNext = () => {
-    if (listRef) listRef.scrollLeft += listRef.clientWidth;
+    if (listRef) {
+      setScrollPosition(() => {
+        const newScrollPosition = listRef.scrollLeft + listRef.clientWidth;
+        listRef.scrollLeft = newScrollPosition;
+        return newScrollPosition;
+      });
+    }
   };
 
   const handleLeft = () => {
-    if (listRef) listRef.scrollLeft -= listRef.clientWidth;
+    if (listRef) {
+      setScrollPosition(() => {
+        const newScrollPosition = listRef.scrollLeft - listRef.clientWidth;
+        listRef.scrollLeft = newScrollPosition;
+        return newScrollPosition;
+      });
+    }
   };
+
+  const disableNext =
+    listRef && listRef.scrollWidth - scrollPosition < listRef.clientWidth;
 
   return (
     <div className='list-arrows__container'>
-      <button className='btn-arrow' onClick={handleLeft}>
+      <motion.button
+        whileHover={{
+          scale: scrollPosition <= 0 ? 1 : 1.2,
+        }}
+        className={`btn-arrow ${
+          scrollPosition <= 0 ? 'btn-arrow--disabled' : ''
+        }`}
+        onClick={handleLeft}
+      >
         <MdNavigateBefore />
-      </button>
-      <button className='btn-arrow' onClick={handleNext}>
+      </motion.button>
+
+      <motion.button
+        whileHover={{
+          scale: disableNext ? 1 : 1.2,
+        }}
+        className={`btn-arrow ${disableNext ? 'btn-arrow--disabled' : ''}`}
+        onClick={handleNext}
+      >
         <MdNavigateNext />
-      </button>
+      </motion.button>
     </div>
   );
 };
