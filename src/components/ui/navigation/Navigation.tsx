c2a93d830d8cdaeca8c27209d3f5635f8844b068
karlghosn1 <karl.ghosn.dev@gmail.com>
2024-12-01 11:07:33 +0200

    navigation code and styling fixes

--- src/components/ui/navigation/Navigation.tsx
+++ src/components/ui/navigation/Navigation.tsx
@@ -4,30 +4,34 @@ import logoImg from '../../../assets/tmdb.svg';
 import { IoMdSearch } from 'react-icons/io';
 import { NAV_LINKS } from '../../../data/navLinks';
 
+const NavigationItem = ({ item }: { item: (typeof NAV_LINKS)[0] }) => {
+  return (
+    <li key={item.link}>
+      <NavLink
+        to={item.link}
+        className={({ isActive }) => (isActive ? classes.active : undefined)}
+      >
+        {item.title}
+      </NavLink>
+    </li>
+  );
+};
+
 export default function Navigation() {
   return (
     <header className={classes.header}>
-      <nav>
+      <nav className={classes.navigation}>
         <NavLink to='/' end>
-          <img src={logoImg} alt='Logo' />
+          <img className={classes.logo} src={logoImg} alt='Logo' />
         </NavLink>
 
-        <ul className={classes['nav-list']}>
+        <ul className={classes.list}>
           {NAV_LINKS.map((item) => (
-            <li key={item.link}>
-              <NavLink
-                to={item.link}
-                className={({ isActive }) =>
-                  isActive ? classes.active : undefined
-                }
-              >
-                {item.title}
-              </NavLink>
-            </li>
+            <NavigationItem key={item.title} item={item} />
           ))}
         </ul>
         <NavLink to='/search' className='link--gradient link--search'>
-          <IoMdSearch className={classes.icon} />
+          <IoMdSearch className={classes['search-icon']} />
           Search
         </NavLink>
       </nav>
